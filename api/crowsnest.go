012723bea865a17121d61346463302ad61f65bfa
Rummy <cerealbh@gmail.com>
2023-06-20 19:26:16 -0500

    Updated api crowsnest
    Added count of jobs with active alerts

--- api/crowsnest.go
+++ api/crowsnest.go
@@ -6,12 +6,21 @@ import (
 )
 
 func (a Api) getStatus(w http.ResponseWriter) {
+	alerts := 0
+	for _, v := range a.nest.Jobs() {
+		if v.History.AlertCount > 0 {
+			alerts++
+		}
+	}
+
 	jsonStatus, err := json.Marshal(struct {
 		StartTime string `json:"startTime"`
 		NumJobs   int    `json:"numJobs"`
+		Alerts    int    `json:"numAlerts"`
 	}{
 		StartTime: a.nest.StartTime.String(),
 		NumJobs:   len(a.nest.Jobs()),
+		Alerts:    alerts,
 	})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
